e39e5612806994a129309b4ae5541e3922559c87
Niko Nevala <niko@nevala.fi>
2013-08-01 16:34:01 +0100

    render asteroid belt in single geometry

--- model/game/terrain/AsteroidBeltFactory.js
+++ model/game/terrain/AsteroidBeltFactory.js
@@ -10,9 +10,6 @@ model.AsteroidBeltFactory = function AsteroidBeltFactory(args)
 
 model.AsteroidBeltFactory.prototype._reset = function()
 {
-    this._segments = 8;
-    this._currentSegment = 1;
-    this._segmentAngle = Math.PI * 2 / this._segments;
     this._asteroids = [];
 }
 
@@ -20,48 +17,28 @@ model.AsteroidBeltFactory.prototype.generateAsteroids = function()
 {
     this._reset();
 
-    var segment, asteroid;
-
-    while (segment = this._getNextSegment()) {
-        var asteroidsCreatedForSegment = 0;
-
-        while (asteroidsCreatedForSegment <= segment.asteroidCount) {
-            try {
-                this._addAsteroid(this._getRandomAsteroidForSegment(segment));
-            } catch (e) {
-                continue;
-            }
-
-            asteroidsCreatedForSegment++;
+    while (this._asteroids.length < this.asteroidCount) {
+        try {
+            this._addAsteroid(this._getRandomAsteroid());
+        } catch (e) {
+            continue;
         }
     }
 
     return this._asteroids;
 }
 
-model.AsteroidBeltFactory.prototype._getNextSegment = function()
-{
-    if (this._currentSegment > this._segments) {
-        return false;
-    }
-
-    return {
-        startAngle: this._currentSegment++ * this._segmentAngle,
-        asteroidCount: this.asteroidCount / this._segments
-    };
-}
-
-model.AsteroidBeltFactory.prototype._getRandomAsteroidForSegment = function(segment)
+model.AsteroidBeltFactory.prototype._getRandomAsteroid = function()
 {
     return new model.Asteroid({
-        position: this._getRandomPositionForSegment(segment),
+        position: this._getRandomPosition(),
         radius: this._getRandomAsteroidRadius()
     });
 }
 
-model.AsteroidBeltFactory.prototype._getRandomPositionForSegment = function(segment)
+model.AsteroidBeltFactory.prototype._getRandomPosition = function()
 {
-    var angle = segment.startAngle + Math.random() * this._segmentAngle;
+    var angle = Math.random() * Math.PI * 2;
     var rand = (Math.random() * 2 - 1) + (Math.random() * 2 - 1) + (Math.random() * 2 - 1);
         rand = rand * 0.1 + 0.5;
     var distance = this.beltRadius + rand * this.beltWidth;
@@ -79,16 +56,6 @@ model.AsteroidBeltFactory.prototype._getRandomAsteroidRadius = function()
 
 model.AsteroidBeltFactory.prototype._addAsteroid = function(newAsteroid)
 {
-//    this._asteroids.forEach(function(existingAsteroid) {
-//        var diffX = Math.abs(newAsteroid.position.x - existingAsteroid.position.x);
-//        var diffY = Math.abs(newAsteroid.position.y - existingAsteroid.position.y);
-//        var distance = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
-//
-//        if (distance < newAsteroid.radius + existingAsteroid.radius) {
-//            throw "Asteroid belt collision detected";
-//        }
-//    });
-
     this._asteroids.push(newAsteroid);
 }
 
--- model/game/terrain/GameTerrain.js
+++ model/game/terrain/GameTerrain.js
@@ -13,36 +13,99 @@ model.GameTerrain.prototype.createRandom = function(container, seed, gameScene, 
     var starField = new model.StarFieldFactory(gameScene, 1000);
     starField.create();
 
-
     gameScene.animators.push(starField);
 
-
     var asteroidFactory = new model.AsteroidBeltFactory({
-        asteroidCount: 10000,
-        minAsteroidRadius: 30,
-        maxAsteroidRadius: 100,
+        beltCentre: { x: 40000, y: 0 },
+        asteroidCount: 20000,
+        minAsteroidRadius: 100,
+        maxAsteroidRadius: 500,
         beltRadius: 20000,
         beltWidth: 50000
     });
     var asteroids = asteroidFactory.generateAsteroids();
 
-    asteroids = asteroids.map(function(asteroid) {
-        return new model.AsteroidParticle(asteroid);
+    var v2 = function(x, y) {
+        return new THREE.Vector2(x, y);
+    }
+    var v3 = function(x, y, z) {
+        return new THREE.Vector3(x, y, z);
+    }
+
+    var textureUvs = [
+        [
+            v2(0, 0),
+            v2(0.25, 0),
+            v2(0.25, 0.25),
+            v2(0, 0.25)
+        ],
+        [
+            v2(0.25, 0),
+            v2(0.5, 0),
+            v2(0.5, 0.25),
+            v2(0.25, 0.25)
+        ],
+        [
+            v2(0.5, 0),
+            v2(0.75, 0),
+            v2(0.75, 0.25),
+            v2(0.5, 0.25)
+        ]
+    ];
+
+    var asteroidBeltGeometry = new THREE.Geometry();
+
+    asteroids.forEach(function(asteroid, index) {
+        asteroidBeltGeometry.vertices.push(
+            v3(asteroid.position.x - asteroid.radius, asteroid.position.y - asteroid.radius, 0),
+            v3(asteroid.position.x + asteroid.radius, asteroid.position.y - asteroid.radius, 0),
+            v3(asteroid.position.x + asteroid.radius, asteroid.position.y + asteroid.radius, 0),
+            v3(asteroid.position.x - asteroid.radius, asteroid.position.y + asteroid.radius, 0)
+
+        );
+
+        var face = new THREE.Face4(index * 4 + 0, index * 4 + 1, index * 4 + 2, index * 4 + 3);
+        asteroidBeltGeometry.faces.push(face);
+
+        asteroidBeltGeometry.faceVertexUvs[0].push(textureUvs[Math.floor(textureUvs.length * Math.random())]);
     });
 
-    var asteroidParticleEmitter = new model.AsteroidParticleEmitter(
-            asteroids,
-            {
-                zoomLevel: gameScene.zoom,
-                texture: THREE.ImageUtils.loadTexture("/terrain/asteroid_lod.png")
-            }
-    );
-    asteroidParticleEmitter.observeZoomLevelChange(dispatcher);
+    var uniforms = {
+        map: {
+            type: 't',
+            value: THREE.ImageUtils.loadTexture("/terrain/asteroid_512.png")
+        }
+    };
 
-    var asteroidBelt = asteroidParticleEmitter.getObject3d();
-    asteroidBelt.position = new THREE.Vector3(0, 0, 0);
-    gameScene.scene.add(asteroidBelt);
-    gameScene.animators.push(asteroidParticleEmitter);
+    var testMaterial = new THREE.ShaderMaterial({
+        uniforms: uniforms,
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+        transparent: true
+    });
+    var asteroidBeltMesh = new THREE.Mesh(asteroidBeltGeometry, testMaterial);
+
+    gameScene.scene.add(asteroidBeltMesh);
 
     return this;
 }
+
+var vertexShader =
+    [
+        "varying vec2 vUv;",
+
+        "void main() {",
+            "vUv = uv;",
+            "gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);",
+        "}"
+    ].join("\n");
+
+var fragmentShader =
+    [
+        "varying vec2 vUv;",
+        "uniform sampler2D map;",
+
+        "void main() {",
+            "gl_FragColor = texture2D(map, vUv);",
+        "}"
+    ].join("\n");
